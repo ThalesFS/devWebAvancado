b87b02c2a94814937a08839bc2b5a13953dabe03
ThalesFS <thalesfs@id.uff.br>
2023-11-21 00:30:03 -0300

    Segundo commit (swagger) para primeira entrega

---
+++ CMS/src/main/java/com/devwebavancado/CMS/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.devwebavancado.CMS;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+//@EnableSwagger2
+@EnableOpenApi
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API Documentation")
+                .description("REST API de um CMS para Desenvolvimento Web Avançado.")
+                .version("1.0")
+                .contact(new Contact("Thales Soares", "https://github.com/ThalesFS", "thalesfs@id.uff.br"))
+                .build();
+    }
+}
